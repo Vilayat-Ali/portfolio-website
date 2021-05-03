763a01e0a95c1f65473f8566f6e785793c2239ce
Syed Vilayat Ali Rizvi <vilayatcodemysite@gmail.com>
2021-05-03 20:30:33 +0530

    Added json files

---
+++ Resources/scripts/author.js
@@ -0,0 +1,9 @@
+fetch('../Resources/info.author.json').then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+}
+)
